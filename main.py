96615dbbe555fc4ff9e3a9df0a550e87ac22a0ff
Hawk2272 <gaurav.chawla@somaiya.edu>
2023-10-14 15:38:38 +0530

    Initial commit and trained the model

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ from keras.preprocessing import image
 import cv2
 import numpy as np
 
-face_classifier = cv2.CascadeClassifier(r'C:\Users\Admin\Desktop\PythonProject\EmotionDetectionCNN\haarcascade_frontalface_default.xml')
-classifier =load_model(r'C:\Users\Admin\Desktop\PythonProject\EmotionDetectionCNN\model.h5')
+face_classifier = cv2.CascadeClassifier(r'C:\Users\gaura\OneDrive - gauravcn3\Desktop\Emotion_Detection_CNN\haarcascade_frontalface_default.xml')
+classifier =load_model(r'C:\Users\gaura\OneDrive - gauravcn3\Desktop\Emotion_Detection_CNN\model.h5')
 
 emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
 
 cap = cv2.VideoCapture(0)
-
-
 
 while True:
     _, frame = cap.read()
@@ -24,9 +22,6 @@ while True:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
         roi_gray = gray[y:y+h,x:x+w]
         roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
-
-
-
         if np.sum([roi_gray])!=0:
             roi = roi_gray.astype('float')/255.0
             roi = img_to_array(roi)
